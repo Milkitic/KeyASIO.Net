80acae6205d5772fc368c93df14e0a6cd148806c
Milkitic <Milkitic@126.com>
2022-06-22 17:30:54 +0800

    Update interface

---
+++ KeyAsio.Gui/Converters/UpperCaseConverter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace KeyAsio.Gui.Converters;
+
+internal class UpperCaseConverter : IValueConverter
+{
+    public object? Convert(object? value, Type targetType, object parameter, CultureInfo culture)
+    {
+        return value?.ToString()?.ToUpper();
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
