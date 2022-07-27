67810d9f2f6be5791ee327d6f93d33f0361c01cf
Milkitic <Milkitic@126.com>
2022-07-27 11:35:10 +0800

    Update afterbuild.ps1

--- KeyAsio.Gui/afterbuild.ps1
+++ KeyAsio.Gui/afterbuild.ps1
@@ -5,9 +5,6 @@ $folder_resources = Join-Path $root "bin" "resources"
 $folder_runtimes = Join-Path $root "bin" "runtimes" 
 
 ./DotNetDllPathPatcher.ps1 $exe_path
-if ($LASTEXITCODE -ne 0) {
-    exit -1
-}
 
 Move-Item $folder_resources $root
 Move-Item $folder_runtimes $root
