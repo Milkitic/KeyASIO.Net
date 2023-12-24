adaa4f9f54a62d31566939ef8bbb785c58efd4c0
Milkitic <Milkitic@126.com>
2023-12-24 17:21:59 +0800

    Add test project

---
+++ Tests/MemoryReadingTest/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+using KeyAsio.MemoryReading;
+using KeyAsio.Shared;
+using KeyAsio.Shared.Realtime;
+using OrtdpLogger = KeyAsio.MemoryReading.Logger;
+
+namespace MemoryReadingTest
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private void App_OnStartup(object sender, StartupEventArgs e)
+        {
+            OrtdpLogger.SetLoggerFactory(LogUtils.LoggerFactory);
+            MemoryScan.MemoryReadObject.PlayerNameChanged += (_, player) => RealtimeModeManager.Instance.Username = player;
+            MemoryScan.MemoryReadObject.ModsChanged += (_, mods) => RealtimeModeManager.Instance.PlayMods = mods;
+            MemoryScan.MemoryReadObject.ComboChanged += (_, combo) => RealtimeModeManager.Instance.Combo = combo;
+            MemoryScan.MemoryReadObject.ScoreChanged += (_, score) => RealtimeModeManager.Instance.Score = score;
+            MemoryScan.MemoryReadObject.PlayingTimeChanged += (_, playTime) => RealtimeModeManager.Instance.LastFetchedPlayTime = playTime;
+            MemoryScan.MemoryReadObject.BeatmapIdentifierChanged += (_, beatmap) => RealtimeModeManager.Instance.Beatmap = beatmap;
+            MemoryScan.MemoryReadObject.OsuStatusChanged += (pre, current) => RealtimeModeManager.Instance.OsuStatus = current;
+            MemoryScan.Start(100);
+        }
+    }
+}
---
+++ Tests/MemoryReadingTest/Charts/PlayTimeChart.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using KeyAsio.MemoryReading;
+using KeyAsio.Shared.Models;
+using LiveChartsCore;
+using LiveChartsCore.Defaults;
+using LiveChartsCore.SkiaSharpView;
+
+namespace MemoryReadingTest.Charts;
+
+public class PlayTimeChartVm : ViewModelBase
+{
+
+    public PlayTimeChartVm()
+    {
+        Series = [new StepLineSeries<TimeSpanPoint> { Values = Collection }];
+    }
+
+    public ISeries[] Series { get; set; }
+
+    public ObservableCollection<TimeSpanPoint> Collection { get; } = [];
+
+    public Axis[] XAxes { get; set; } =
+    [
+        new TimeSpanAxis(TimeSpan.FromMilliseconds(1), timeSpan => $"{timeSpan.TotalMilliseconds}ms")
+    ];
+}
+
+/// <summary>
+/// PlayTimeChart.xaml 的交互逻辑
+/// </summary>
+public partial class PlayTimeChart : UserControl
+{
+    private readonly PlayTimeChartVm _viewModel;
+
+    public PlayTimeChart()
+    {
+        InitializeComponent();
+        DataContext = _viewModel = new PlayTimeChartVm();
+
+        MemoryScan.MemoryReadObject.PlayingTimeChanged += MemoryReadObject_PlayingTimeChanged;
+    }
+
+    private void MemoryReadObject_PlayingTimeChanged(int oldValue, int newValue)
+    {
+        _viewModel.Collection.Add(new TimeSpanPoint(TimeSpan.FromMicroseconds(newValue), newValue));
+    }
+}
