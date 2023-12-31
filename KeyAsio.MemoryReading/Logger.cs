1bf6e78b1800b9a71af4d6b4849099b094262d72
Milkitic <Milkitic@126.com>
2023-12-31 13:50:15 +0800

    Logger rename

--- KeyAsio.MemoryReading/Logger.cs
+++ KeyAsio.MemoryReading/Logger.cs
@@ -4,30 +4,30 @@ namespace KeyAsio.MemoryReading;
 
 public static class Logger
 {
-    private static ILoggerFactory _loggerFactory;
+    private static ILoggerFactory? _loggerFactory;
     public static void SetLoggerFactory(ILoggerFactory loggerFactory) => _loggerFactory = loggerFactory;
 
     public static void Info(string message)
     {
         if (_loggerFactory is null) Console.WriteLine(message);
-        else _loggerFactory.CreateLogger("ORTDP").LogInformation(message);
+        else _loggerFactory.CreateLogger("KEYAISO_MEM").LogInformation(message);
     }
 
     public static void Debug(string message)
     {
         if (_loggerFactory is null) Console.WriteLine(message);
-        else _loggerFactory.CreateLogger("ORTDP").LogDebug(message);
+        else _loggerFactory.CreateLogger("KEYAISO_MEM").LogDebug(message);
     }
 
     public static void Error(string message)
     {
         if (_loggerFactory is null) Console.WriteLine(message);
-        else _loggerFactory.CreateLogger("ORTDP").LogError(message);
+        else _loggerFactory.CreateLogger("KEYAISO_MEM").LogError(message);
     }
 
     public static void Warn(string message)
     {
         if (_loggerFactory is null) Console.WriteLine(message);
-        else _loggerFactory.CreateLogger("ORTDP").LogWarning(message);
+        else _loggerFactory.CreateLogger("KEYAISO_MEM").LogWarning(message);
     }
 }
