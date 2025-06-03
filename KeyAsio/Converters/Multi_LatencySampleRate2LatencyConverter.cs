54b199ec690cf95fa87ec5225d3806b1afaf7208
Milkitic <Milkitic@126.com>
2025-06-03 17:46:18 +0800

    feat(KeyAsio): ✨ 添加 KeyAsio 项目及基础结构
    新增 KeyAsio 项目，包括主窗口、应用程序和资源字典。
    实现了基本的应用程序结构和界面布局，支持音频设备的配置和信息展示。

---
+++ KeyAsio/Converters/Multi_LatencySampleRate2LatencyConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using Avalonia.Data.Converters;
+
+namespace KeyAsio.Converters;
+
+internal class Multi_LatencySampleRate2LatencyConverter : IMultiValueConverter
+{
+    public object? Convert(IList<object?> values, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (values is [int playbackLatency, int sampleRate])
+        {
+            var latency = 1000d / sampleRate * playbackLatency;
+            return latency;
+        }
+
+        return double.NaN;
+    }
+}
---
+++ KeyAsio/MainWindow.axaml.cs
@@ -0,0 +1,20 @@
+using Avalonia.Interactivity;
+using SukiUI.Controls;
+
+namespace KeyAsio;
+
+public partial class MainWindow : SukiWindow
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void btnAsioControlPanel_OnClick(object? sender, RoutedEventArgs e)
+    {
+    }
+
+    private void btnDisposeDevice_OnClick(object? sender, RoutedEventArgs e)
+    {
+    }
+}
