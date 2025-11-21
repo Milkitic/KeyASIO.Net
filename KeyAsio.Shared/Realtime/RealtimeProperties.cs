483030deae0c95b94a01190e3fd39683aabd8e7b
Milkitic <Milkitic@126.com>
2025-11-21 16:35:05 +0800

    refactor(Realtime): 将RealtimeProperties类提取到单独文件
    重构代码结构，将RealtimeProperties类从RealtimeModeManager.cs中分离到独立的RealtimeProperties.cs文件，提高代码可维护性和组织性

---
+++ KeyAsio.Shared/Realtime/RealtimeProperties.cs
@@ -0,0 +1,147 @@
+using System.Diagnostics;
+using System.Text;
+using KeyAsio.MemoryReading;
+using KeyAsio.Shared.Models;
+using KeyAsio.Shared.Utils;
+using OsuMemoryDataProvider;
+
+namespace KeyAsio.Shared.Realtime;
+
+public class RealtimeProperties : ViewModelBase
+{
+    public Func<int, int, ValueTask>? OnComboChanged;
+    public Func<Mods, Mods, ValueTask>? OnPlayModsChanged;
+    public Func<int, int, bool, ValueTask>? OnFetchedPlayTimeChanged;
+    public Func<OsuMemoryStatus, OsuMemoryStatus, ValueTask>? OnStatusChanged;
+    public Func<BeatmapIdentifier, BeatmapIdentifier, ValueTask>? OnBeatmapChanged;
+
+    private readonly AppSettings _appSettings;
+    private readonly Stopwatch _playTimeStopwatch = new();
+
+    private int _playTime;
+    private string? _username;
+    private Mods _playMods;
+    private int _lastFetchedPlayTime;
+    private int _combo;
+    private int _score;
+    private OsuMemoryStatus _osuStatus;
+    private BeatmapIdentifier _beatmap;
+
+    public RealtimeProperties(AppSettings appSettings)
+    {
+        _appSettings = appSettings;
+    }
+
+    public bool IsStarted { get; set; }
+    public bool IsReplay { get; set; }
+
+    public string? Username
+    {
+        get => _username;
+        set
+        {
+            if (_username == value) return;
+            _username = value;
+            if (!string.IsNullOrEmpty(value))
+            {
+                _appSettings.PlayerBase64 = EncodeUtils.GetBase64String(value, Encoding.ASCII);
+            }
+
+            OnPropertyChanged();
+        }
+    }
+
+    public Mods PlayMods
+    {
+        get => _playMods;
+        set
+        {
+            var val = _playMods;
+            if (SetField(ref _playMods, value))
+            {
+                OnPlayModsChanged?.Invoke(val, value);
+            }
+        }
+    }
+
+    public int PlayTime
+    {
+        get => _playTime;
+        set
+        {
+            value += _appSettings.RealtimeOptions.RealtimeModeAudioOffset + (int)_playTimeStopwatch.ElapsedMilliseconds;
+            var val = _playTime;
+            if (SetField(ref _playTime, value))
+            {
+                OnFetchedPlayTimeChanged?.Invoke(val, value, false);
+            }
+            else
+            {
+                OnFetchedPlayTimeChanged?.Invoke(val, value, true);
+            }
+        }
+    }
+
+    public int LastFetchedPlayTime
+    {
+        get => _lastFetchedPlayTime;
+        set
+        {
+            if (SetField(ref _lastFetchedPlayTime, value))
+            {
+                _playTimeStopwatch.Restart();
+            }
+            else
+            {
+                _playTimeStopwatch.Reset();
+            }
+
+            PlayTime = value;
+        }
+    }
+
+    public int Combo
+    {
+        get => _combo;
+        set
+        {
+            var val = _combo;
+            if (SetField(ref _combo, value))
+            {
+                OnComboChanged?.Invoke(val, value);
+            }
+        }
+    }
+
+    public int Score
+    {
+        get => _score;
+        set => SetField(ref _score, value);
+    }
+
+    public OsuMemoryStatus OsuStatus
+    {
+        get => _osuStatus;
+        set
+        {
+            var val = _osuStatus;
+            if (SetField(ref _osuStatus, value))
+            {
+                OnStatusChanged?.Invoke(val, value);
+            }
+        }
+    }
+
+    public BeatmapIdentifier Beatmap
+    {
+        get => _beatmap;
+        set
+        {
+            var val = _beatmap;
+            if (SetField(ref _beatmap, value))
+            {
+                OnBeatmapChanged?.Invoke(val, value);
+            }
+        }
+    }
+}
