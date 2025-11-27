d785eeb5fcf25c39c09845ac9f19eac990f441fc
Milkitic <Milkitic@126.com>
2025-11-27 17:18:08 +0800

    feat(UI): 添加音频设置界面及功能绑定
    - 在MainWindowViewModel中添加音频相关属性
    - 实现音频设置界面的布局和功能绑定
    - 优化主界面布局和样式
    - 添加设置应用按钮及命令绑定

--- KeyAsio/ViewModels/MainWindowViewModel.cs
+++ KeyAsio/ViewModels/MainWindowViewModel.cs
@@ -1,4 +1,5 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 
 namespace KeyAsio.ViewModels;
 
@@ -9,4 +10,19 @@ public partial class MainWindowViewModel
 
     [ObservableProperty]
     public partial bool RealtimeMode { get; set; }
+
+    [ObservableProperty]
+    public partial double TargetBufferSize { get; set; } = 3;
+
+    [ObservableProperty]
+    public partial bool IsExclusiveMode { get; set; } = true;
+
+    [ObservableProperty]
+    public partial bool IsLimiterEnabled { get; set; } = true;
+
+    [RelayCommand]
+    private void ApplyAudioSettings()
+    {
+        // TODO: Apply settings to the audio engine
+    }
 }
