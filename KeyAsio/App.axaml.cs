932f164f68d5d7cb3d1bc019f7c6a0d7e8776b86
Milkitic <Milkitic@126.com>
2025-11-28 17:11:12 +0800

    feat(UI): 为设置页面添加图标以提升视觉体验
    在音频引擎、设置和同步模式页面中，为各个设置项添加了Material图标，使界面更加直观美观。同时更新了调试工具配置，在DEBUG模式下启用开发者工具。

--- KeyAsio/App.axaml.cs
+++ KeyAsio/App.axaml.cs
@@ -12,6 +12,10 @@ public partial class App : Application
     public override void Initialize()
     {
         AvaloniaXamlLoader.Load(this);
+
+#if DEBUG
+        this.AttachDeveloperTools();
+#endif
     }
 
     public override void OnFrameworkInitializationCompleted()
