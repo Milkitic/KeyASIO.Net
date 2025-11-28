df931056718dc0117e813abb41ab92e5e4b1bf72
Milkitic <Milkitic@126.com>
2025-11-28 09:55:50 +0800

    refactor(ui): 将页面内容提取到独立文件以提升可维护性

---
+++ KeyAsio/Views/Pages/SettingsPage.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace KeyAsio.Views.Pages;
+
+public partial class SettingsPage : UserControl
+{
+    public SettingsPage()
+    {
+        InitializeComponent();
+    }
+}
