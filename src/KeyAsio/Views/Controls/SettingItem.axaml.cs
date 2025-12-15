543434d0fc2f489c5b2c3e0e1f5bb81eff5d396a
Milkitic <Milkitic@126.com>
2025-12-15 10:20:05 +0800

    feat(ui): add reusable StatBadge and SettingItem controls and refactor settings pages
    refactor settings pages to use new reusable controls for consistent styling
    add StatBadge component for displaying key-value stats
    add SettingItem component for standardized setting rows with icons
    update all settings pages to use new components

---
+++ src/KeyAsio/Views/Controls/SettingItem.axaml.cs
@@ -0,0 +1,49 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Material.Icons;
+
+namespace KeyAsio.Views.Controls;
+
+public partial class SettingItem : UserControl
+{
+    public SettingItem()
+    {
+        InitializeComponent();
+    }
+
+    public static readonly StyledProperty<MaterialIconKind> IconProperty =
+        AvaloniaProperty.Register<SettingItem, MaterialIconKind>(nameof(Icon));
+
+    public MaterialIconKind Icon
+    {
+        get => GetValue(IconProperty);
+        set => SetValue(IconProperty, value);
+    }
+
+    public static readonly StyledProperty<string> HeaderProperty =
+        AvaloniaProperty.Register<SettingItem, string>(nameof(Header));
+
+    public string Header
+    {
+        get => GetValue(HeaderProperty);
+        set => SetValue(HeaderProperty, value);
+    }
+
+    public static readonly StyledProperty<string> DescriptionProperty =
+        AvaloniaProperty.Register<SettingItem, string>(nameof(Description));
+
+    public string Description
+    {
+        get => GetValue(DescriptionProperty);
+        set => SetValue(DescriptionProperty, value);
+    }
+
+    public static readonly StyledProperty<object> ActionContentProperty =
+        AvaloniaProperty.Register<SettingItem, object>(nameof(ActionContent));
+
+    public object ActionContent
+    {
+        get => GetValue(ActionContentProperty);
+        set => SetValue(ActionContentProperty, value);
+    }
+}
---
+++ src/KeyAsio/Views/Controls/StatBadge.axaml.cs
@@ -0,0 +1,30 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+
+namespace KeyAsio.Views.Controls;
+
+public partial class StatBadge : UserControl
+{
+    public StatBadge()
+    {
+        InitializeComponent();
+    }
+
+    public static readonly StyledProperty<string> LabelProperty =
+        AvaloniaProperty.Register<StatBadge, string>(nameof(Label));
+
+    public string Label
+    {
+        get => GetValue(LabelProperty);
+        set => SetValue(LabelProperty, value);
+    }
+
+    public static readonly StyledProperty<object> ValueProperty =
+        AvaloniaProperty.Register<StatBadge, object>(nameof(Value));
+
+    public object Value
+    {
+        get => GetValue(ValueProperty);
+        set => SetValue(ValueProperty, value);
+    }
+}
