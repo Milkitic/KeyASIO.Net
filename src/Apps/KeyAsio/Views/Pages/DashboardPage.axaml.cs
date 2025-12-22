ad6fb0e9ffccae59f61ee166bae2c52d53bbf8d9
Milkitic <Milkitic@126.com>
2025-12-22 17:59:18 +0800

    refactor(DashboardPage): simplify mix switch styling and add dynamic color update
    Move color styling logic from XAML to code-behind for better maintainability
    Add dynamic color update based on IsMixModeTagPro property

--- src/Apps/KeyAsio/Views/Pages/DashboardPage.axaml.cs
+++ src/Apps/KeyAsio/Views/Pages/DashboardPage.axaml.cs
@@ -1,4 +1,6 @@
-using Avalonia.Controls;
+﻿using Avalonia.Controls;
+using Avalonia.Media;
+using KeyAsio.ViewModels;
 
 namespace KeyAsio.Views.Pages;
 
@@ -7,5 +9,33 @@ public partial class DashboardPage : UserControl
     public DashboardPage()
     {
         InitializeComponent();
+    }
+
+    protected override void OnDataContextChanged(EventArgs e)
+    {
+        base.OnDataContextChanged(e);
+        if (DataContext is MainWindowViewModel vm)
+        {
+            UpdateSwitchColor(vm.IsMixModeTagPro);
+            vm.PropertyChanged += (sender, args) =>
+            {
+                if (args.PropertyName == nameof(MainWindowViewModel.IsMixModeTagPro))
+                {
+                    UpdateSwitchColor(vm.IsMixModeTagPro);
+                }
+            };
+        }
+    }
+
+    private void UpdateSwitchColor(bool isPro)
+    {
+        if (isPro)
+        {
+            MixSwitch.Resources["SukiPrimaryColor"] = Color.Parse("#FFD700");
+        }
+        else
+        {
+            MixSwitch.Resources.Remove("SukiPrimaryColor");
+        }
     }
 }
