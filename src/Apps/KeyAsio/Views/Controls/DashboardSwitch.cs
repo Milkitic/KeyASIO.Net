bc83e9b39ee1eaa15ccabff6e58c31dc39a1bf1e
Milkitic <Milkitic@126.com>
2025-12-23 17:29:27 +0800

    feat(dashboard): add custom DashboardSwitch control with styles
    - Create new DashboardSwitch control with pro/corrupt states and mix mode support
    - Implement styles for DashboardSwitch including premium gold styling
    - Replace existing ToggleSwitch in DashboardPage with new DashboardSwitch
    - Add preview examples in DashboardSwitchStyles.axaml

---
+++ src/Apps/KeyAsio/Views/Controls/DashboardSwitch.cs
@@ -0,0 +1,83 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Controls.Metadata;
+using Avalonia.Controls.Primitives;
+using Avalonia.Data;
+
+namespace KeyAsio.Views.Controls;
+
+[PseudoClasses(":pro", ":corrupt", ":checked")]
+public class DashboardSwitch : TemplatedControl
+{
+    public static readonly StyledProperty<bool> IsCheckedProperty =
+        AvaloniaProperty.Register<DashboardSwitch, bool>(nameof(IsChecked), defaultBindingMode: BindingMode.TwoWay);
+
+    public bool IsChecked
+    {
+        get => GetValue(IsCheckedProperty);
+        set => SetValue(IsCheckedProperty, value);
+    }
+
+    public static readonly StyledProperty<bool> IsProProperty =
+        AvaloniaProperty.Register<DashboardSwitch, bool>(nameof(IsPro));
+
+    public bool IsPro
+    {
+        get => GetValue(IsProProperty);
+        set => SetValue(IsProProperty, value);
+    }
+
+    public static readonly StyledProperty<bool> IsCorruptProperty =
+        AvaloniaProperty.Register<DashboardSwitch, bool>(nameof(IsCorrupt));
+
+    public bool IsCorrupt
+    {
+        get => GetValue(IsCorruptProperty);
+        set => SetValue(IsCorruptProperty, value);
+    }
+
+    public static readonly StyledProperty<string?> MixModeDisplayNameProperty =
+        AvaloniaProperty.Register<DashboardSwitch, string?>(nameof(MixModeDisplayName));
+
+    public string? MixModeDisplayName
+    {
+        get => GetValue(MixModeDisplayNameProperty);
+        set => SetValue(MixModeDisplayNameProperty, value);
+    }
+
+    public static readonly StyledProperty<string?> MixModeTagProperty =
+        AvaloniaProperty.Register<DashboardSwitch, string?>(nameof(MixModeTag));
+
+    public string? MixModeTag
+    {
+        get => GetValue(MixModeTagProperty);
+        set => SetValue(MixModeTagProperty, value);
+    }
+
+    public static readonly StyledProperty<bool> HasMixModeTagProperty =
+        AvaloniaProperty.Register<DashboardSwitch, bool>(nameof(HasMixModeTag));
+
+    public bool HasMixModeTag
+    {
+        get => GetValue(HasMixModeTagProperty);
+        set => SetValue(HasMixModeTagProperty, value);
+    }
+
+    protected override void OnPropertyChanged(AvaloniaPropertyChangedEventArgs change)
+    {
+        base.OnPropertyChanged(change);
+
+        if (change.Property == IsProProperty)
+        {
+            PseudoClasses.Set(":pro", change.GetNewValue<bool>());
+        }
+        else if (change.Property == IsCorruptProperty)
+        {
+            PseudoClasses.Set(":corrupt", change.GetNewValue<bool>());
+        }
+        else if (change.Property == IsCheckedProperty)
+        {
+            PseudoClasses.Set(":checked", change.GetNewValue<bool>());
+        }
+    }
+}
