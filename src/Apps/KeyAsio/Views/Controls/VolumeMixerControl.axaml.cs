a306f629f27242b5f53f07e2362fd3a3a8d391b1
Milkitic <Milkitic@126.com>
2025-12-26 11:39:34 +0800

    feat(volume-mixer): extract volume control into separate component
    Refactor volume control UI from MainWindow into reusable VolumeMixerControl

---
+++ src/Apps/KeyAsio/Views/Controls/VolumeMixerControl.axaml.cs
@@ -0,0 +1,27 @@
+﻿using Avalonia.Controls;
+using Avalonia.Input;
+using Avalonia.Markup.Xaml;
+
+namespace KeyAsio.Views.Controls;
+
+public partial class VolumeMixerControl : UserControl
+{
+    public VolumeMixerControl()
+    {
+        InitializeComponent();
+    }
+
+    private void InitializeComponent()
+    {
+        AvaloniaXamlLoader.Load(this);
+    }
+
+    private void Slider_PointerWheelChanged(object? sender, PointerWheelEventArgs e)
+    {
+        if (sender is Slider slider)
+        {
+            slider.Value += e.Delta.Y;
+            e.Handled = true;
+        }
+    }
+}
